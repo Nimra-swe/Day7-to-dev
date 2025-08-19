371d046804771ac4addbfc50aa70f0be53bdf225
C:/Program Files/Git/Nimra-swe <nimrasattar6050@gmail.com>
2025-08-19 05:27:10 -0700

    Basic javascript

---
+++ script.js
@@ -0,0 +1,68 @@
+// Variable and operators
+let name = "nimra"
+console.log(name);
+const year = 2006
+console.log(year);
+let age = 16
+console.log(age);
+
+// Variable naming and constant/
+const pi =3.141;
+console.log(pi);
+
+// Arthematic operator//
+ p = 12;
+ q = 5;
+ r = p + q;
+ console.log(r);
+
+ m = 12;
+ n = 5;
+ o = m - n;
+ console.log(o);
+
+ g = 12;
+ h = 5;
+ i = g * h;
+ console.log(i);
+
+ d = 12;
+ e = 5;
+ f = d % e;
+ console.log(f);
+
+ a = 2;
+ b = 2;
+ c = a ** b;
+ console.log(c);
+/////////////////
+let counter = 2;
+counter++;
+alert( counter );
+//-----------//
+ let num = 2;
+ num--;
+ alert( num );
+
+
+//  Numbers in javascript
+ let a = 25;
+ let b = 0.124;
+ c= a + b;
+ console.log(c);
+
+let s = (10 + "5");
+console.log(s);
+
+let x = "hello" / 123;
+isNaN(x);
+
+// basic operators and maths
+let apples = "2";
+let oranges = "3";
+alert( apples + oranges );
+
+// Assigenment operator
+let u = 3; 
+let v = 4; 
+u = v;
